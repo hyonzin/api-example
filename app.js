48d00a2806ca0d2a1bcd15fccb68eacb9fbcba68
hyonzin <hyonzin@outlook.kr>
2018-12-07 23:01:45 +0900

    cool

--- app.js
+++ app.js
@@ -1,11 +1,71 @@
 var express = require('express');
 var app = express();
+var port_num = 3000;
 
-app.get('/', function (req, res) {
-		  res.json({msg: 'Hello World!'});
+
+app.use(express.static(__dirname + '/static')); 
+
+
+/* Coffee */
+
+var coffee = [];
+
+app.get('/api/v1.0/coffee', function (req, res) {
+	// get all coffees
+	res.json({coffee: coffee, error: 0});
 });
 
-app.listen(3000, function () {
-		  console.log('Example app listening on port 3000!');
+app.post('/api/v1.0/coffee/:name', function (req, res) {
+	var name = req.params.name;
+
+	// check value
+	if (name == null || name == "") {
+		res.json({error: -1});
+		return;
+	}
+
+	// find the coffee
+	var idx = coffee.indexOf(name);
+
+	// return if it already exists
+	if (idx != -1) {
+		res.json({error: -2});
+		return;
+	}
+
+	// add new coffee
+	coffee.push(name);
+
+	// return code
+	res.json({error: 0});
+});
+
+app.delete('/api/v1.0/coffee/:name', function (req, res) {
+	var name = req.params.name;
+
+	// check value
+	if (name == null || name == "") {
+		res.json({error: -1});
+		return;
+	}
+
+	// find the coffee
+	var idx = coffee.indexOf(name);
+
+	// return if it doesn't exist
+	if (idx == -1) {
+		res.json({error: -2});
+		return;
+	}
+
+	// delete the coffee
+	coffee.splice(idx, 1);
+
+	// return code
+	res.json({error: 0});
+});
+
+app.listen(port_num, function () {
+	console.log('Listening on port ' + port_num);
 });
 
